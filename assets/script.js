ab089539e8904d6ee79fb199f1242a5a061d6e34
Trymne <tsnps3@gmail.com>
2022-04-19 12:24:16 +0200

    more javascript ++

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,13 @@
 const frame1 = document.querySelector('#frame1');
-      const riverCover = document.querySelector('#cover');
-      const body = document.querySelector('body');
-      const sun = document.querySelector('#sun');
-      const box1Scroller = scrollama();
-      const wellScroller = scrollama();
+const frame2 = document.querySelector('#frame2');
+const frame3 = document.querySelector('#frame3');
+const frame4 = document.querySelector('#frame4');
+const river = document.querySelector('#river');
+const riverCover = document.querySelector('#cover');
+const body = document.querySelector('body');
+const sun = document.querySelector('#sun');
+const box1Scroller = scrollama();
+const box2Scroller = scrollama();
 
 box1Scroller
     .setup({
@@ -11,14 +15,38 @@ box1Scroller
     })
     .onStepEnter(() => {
         frame1.classList.remove('hidden');
+        frame2.classList.add('hidden');
+        frame4.classList.add('hidden');
         body.classList.remove('african');
         sun.classList.remove('sun-african');
+        river.classList.remove('hidden');
         riverCover.classList.add('hidden'); 
     })
     .onStepExit(() => {
         frame1.classList.add('hidden');
+        frame2.classList.remove('hidden');
         body.classList.add('african');
         sun.classList.add('sun-african');
         riverCover.classList.remove('hidden');
+        console.log('hei');
+    })
+
+    box2Scroller
+    .setup({
+        step: '.box2'
+    })
+    .onStepEnter(() => {
+        frame3.classList.remove('hidden');
+        frame2.classList.add('hidden');
+        frame4.classList.add('hidden');
+        body.classList.remove('african');
+        river.classList.add('hidden');
+        sun.classList.remove('sun-african'); 
+    })
+    .onStepExit(() => {
+        frame3.classList.add('hidden');
+        frame4.classList.remove('hidden');
+        body.classList.add('african');
+        sun.classList.add('sun-african');
         console.log('hei');
     })
